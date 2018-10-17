3354bb6e276c55e182f89bb35c12669ae9eaa709
Sandun_m <sandunmayura@gmail.com>
2018-10-17 14:39:37 +0530

    initial Commit

---
+++ src/main/java/com/flyway/KPDB/KpDbApplication.java
@@ -0,0 +1,12 @@
+package com.flyway.KPDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpDbApplication.class, args);
+	}
+}
---
+++ src/main/resources/db/migration/V1__createInitialTable.sql
@@ -0,0 +1,20 @@
+CREATE TABLE teams
+(team_id int NOT NULL AUTO_INCREMENT,
+team_name varchar(50),
+no_of_team_members int(2),
+PRIMARY KEY (team_id));
+
+
+
+CREATE TABLE report_data
+(data_id int NOT NULL AUTO_INCREMENT,
+team_id int(10),
+qa_bug int(2),
+client_bug int(2),
+ontime_delivered int(2),
+not_delivered int(2),
+misses int(2),
+date date,
+no_of_week int(2),
+PRIMARY KEY (data_id),
+FOREIGN KEY (team_id) REFERENCES teams(team_id));
