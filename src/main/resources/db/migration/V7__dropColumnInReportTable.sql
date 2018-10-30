e60ce48f9c796aac4ecd8da19857847367655dd4
Sandun_mayura <iit140013@std.uwu.ac.lk>
2018-10-30 17:02:37 +0530

    drop and add new baseline table

---
+++ src/main/resources/db/migration/V7__dropColumnInReportTable.sql
@@ -0,0 +1,8 @@
+ALTER TABLE report_data DROP COLUMN qa_baseline ,
+     DROP COLUMN qa_baseline_target ,
+    DROP COLUMN  not_delivered_baseline ,
+   DROP COLUMN  not_delivered_baseline_target ,
+    DROP COLUMN  total_bug_avg ,
+   DROP COLUMN   total_bug_avg_target ,
+    DROP COLUMN  misses_baseline ,
+    DROP COLUMN  misses_baseline_target ;
